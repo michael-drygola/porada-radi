3c1cba0239296fedfb6edd7c6aed5ec051c37359
mdrygola <michail@twodollarweb.com>
2013-12-29 10:05:32 +0200

    Add fields and accessors to Vote entity

--- src/main/java/wissolsoft/porada/webapp/model/Vote.java
+++ src/main/java/wissolsoft/porada/webapp/model/Vote.java
@@ -6,14 +6,55 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.appfuse.model.User;
+
 @Entity
 @Table(name="vote")
 public class Vote {
 
-	@Id
-	@GeneratedValue
-	private long id;
+    public enum Value {
 
-	@ManyToOne
-	private Law law;
+    }
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @ManyToOne
+    private Law law;
+
+    @ManyToOne
+    private User user;
+
+    private Value value;
+
+    public long getId() {
+        return id;
+    }
+
+    public Law getLaw() {
+        return law;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setLaw(Law law) {
+        this.law = law;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+
 }
