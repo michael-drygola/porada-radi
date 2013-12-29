b29b2c3827ee0d436fd1f73150009416f794bf2d
mdrygola <michail@twodollarweb.com>
2013-12-29 13:13:25 +0200

    Remove hardcoded test data

--- src/main/java/wissolsoft/porada/webapp/controller/LawsController.java
+++ src/main/java/wissolsoft/porada/webapp/controller/LawsController.java
@@ -67,21 +67,4 @@ public class LawsController {
         return "details";
     }
 
-    @PostConstruct
-    public void insertTestData() {
-        final Law testLaw1 = new Law();
-        testLaw1.setCaption("Проект Закону про усунення негативних наслідків та недопущення переслідування та покарання осіб з приводу подій, які мали місце під час проведення мирних зібрань");
-        testLaw1.setDate(new Date());
-        testLaw1.setId(3787);
-        testLaw1.setLink("http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=49375");
-        final Law testLaw2 = new Law();
-        testLaw2.setCaption("Проект Закону про внесення змін до Закону України 'Про спеціальну (вільну) економічну зону 'Порто-франко' на території Одеського морського торговельного порту' (щодо регулювання інвестиційної діяльності на території СЕЗ 'Порто-франко')");
-        testLaw2.setDate(new Date());
-        testLaw2.setId(3788);
-        testLaw2.setLink("http://w1.c1.rada.gov.ua/pls/zweb2/webproc4_1?pf3511=49371");
-
-        lawsManager.insertLaw(testLaw1);
-        lawsManager.insertLaw(testLaw2);
-    }
-
 }
